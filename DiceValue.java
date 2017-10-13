d58058a7721de75f8b74ed66e0b3cbe1ca20cfb5
rawatshalini <30823647+rawatshalini@users.noreply.github.com>
2017-10-12 23:47:53 -0700

    DiceValue.java change for getting spade in betting and rolling

--- DiceValue.java
+++ DiceValue.java
@@ -24,7 +24,7 @@ public enum DiceValue {
 	public static DiceValue getRandom() {
 		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
 		//bug 3: Spade card should display while betting and rolling
-		return values()[random];
+		return values()[random + 1];
 	}
 	
 }
